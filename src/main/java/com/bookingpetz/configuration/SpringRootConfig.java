449bb28f124643f0ee4fec1190be379a05e6cf3c
burakzengin <brakzengin@gmail.com>
2019-07-04 12:31:37 +0200

    init commit

---
+++ src/main/java/com/bookingpetz/configuration/SpringRootConfig.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookingpetz.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Elidor
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.bookingpetz"})
+public class SpringRootConfig {
+
+}
