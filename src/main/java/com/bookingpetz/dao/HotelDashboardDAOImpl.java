853026bb4aeba4cda677ca92700ec2c9002c275f
burakzengin <burakzengin@172.20.10.5>
2019-09-05 22:56:24 +0200

    update hotel

--- src/main/java/com/bookingpetz/dao/HotelDashboardDAOImpl.java
+++ src/main/java/com/bookingpetz/dao/HotelDashboardDAOImpl.java
@@ -24,7 +24,7 @@ public class HotelDashboardDAOImpl implements HotelDashboardDAO {
     @Override
     public boolean registerHotel(User user, String token) {
 
-        HttpResponse<JsonNode> response = Unirest.post("http://localhost:8080/BookingPetsREST/webapi/gateway/auth/registerHotel")
+        HttpResponse<JsonNode> response = Unirest.post("https://bookingpetswebservice.herokuapp.com/webapi/gateway/auth/registerHotel")
                 .header("Content-type", "application/json")
                 .header("Authorization", "Bearer " + token)
                 .body(new Gson().toJson(user))
@@ -35,7 +35,7 @@ public class HotelDashboardDAOImpl implements HotelDashboardDAO {
 
     @Override
     public boolean updateHotel(User user, String token) {
-        HttpResponse<JsonNode> response = Unirest.post("http://localhost:8080/BookingPetsREST/webapi/gateway/auth/updateHotel")
+        HttpResponse<JsonNode> response = Unirest.post("https://bookingpetswebservice.herokuapp.com/webapi/gateway/auth/updateHotel")
                 .header("Content-type", "application/json")
                 .header("Authorization", "Bearer " + token)
                 .body(new Gson().toJson(user))
